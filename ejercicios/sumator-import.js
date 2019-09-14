067f1007660b73847813c60ec2617470e1ae15a5
Tochituru <rotrupo@gmail.com>
2019-09-14 17:39:27 -0300

    Dependencies (modules and packages)

---
+++ ejercicios/sumator-import.js
@@ -0,0 +1,3 @@
+const suma = require('../modules/sumator-export');
+
+console.log(suma(5,10));
---
+++ modules/sumator-export.js
@@ -0,0 +1,7 @@
+//[Esta función reciba cosas y entrega otra].
+//Recibe una lista de parámetros y entrega el resultado de la suma.
+
+const sumator = (a, b) => a+b;
+
+
+module.exports = sumator;
