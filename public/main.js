c1f727ed5690289d58d6c6aa1e305a970dead399
Tochituru <rotrupo@gmail.com>
2019-09-16 19:01:36 -0300

    Fetch básico from localhost

--- public/main.js
+++ public/main.js
@@ -1,14 +1,21 @@
-const listItem = document.createElement('li');
-let list = document.getElementById('list');
-listItem.innerHTML = 'sarasa';
-list.appendChild(listItem);
-
 const printProducts = (data) => {
+    console.log(data);
     
+    const container = document.getElementById('productsList');
+    data.forEach(e => {
+        container.innerHTML += productItem(e); 
+    });
 }
+
+const productItem = (item) => `
+    <li class="item-style" id="pepito">
+    <p>${item.computer} </p>
+    <span>${item.ram} </span>
+    </li>
+`;
 
 const initialize = () => { 
     fetch('/api/products')
         .then(res => res.json())
-        .then(res => printProducts (res));
+        .then(res => printProducts (res.products));
         };
